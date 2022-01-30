bc1367cd7d1c03490ca56d2843789f1f286b247b
Hilmi Abou-Saleh <2ndgenairborne@gmail.com>
2022-01-30 10:44:23 -0700

    Add test hook

--- Source/index.ts
+++ Source/index.ts
@@ -4,14 +4,22 @@ import Settings from "./Utils/Settings";
 import PreloadTemplates from "./PreloadTemplates";
 
 Hooks.once("init", async () => {
-   Settings.Get().RegisterSettings();
-   await PreloadTemplates();
+	Logger.Log("Initializing....")
+	Settings.Get().RegisterSettings();
+	await PreloadTemplates();
 });
 
 Hooks.once("setup", () => {
-   Logger.Log("Template module is being setup.")
+	Logger.Log("Template module is being setup.")
 });
 
 Hooks.once("ready", () => {
-   Logger.Ok("Template module is now ready.");
+	Logger.Ok("Template module is now ready.");
 });
+
+Hooks.on("renderChatMessage", (message: ChatMessage, html: any, data: any) => {
+	if (message.roll?.result != null) {
+		const result: string = message.roll.result
+		Logger.Log(result)
+	}
+})
